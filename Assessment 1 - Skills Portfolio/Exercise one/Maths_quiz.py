6f17c0cd35baca6d90084e7adc0565afd85684fd
Luna Alsalman <manahel1506@gmail.com>
2025-10-27 00:38:51 +0400

    application template created
    started with the basic structure of the application

---
+++ Assessment 1 - Skills Portfolio/Exercise one/Maths_quiz.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+# === Settings ===
+WINDOW_WIDTH = 960
+WINDOW_HEIGHT = 540
+# === App ===
+root = tk.Tk()
+root.title("Maths Quiz")
+root.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
+root.mainloop()
