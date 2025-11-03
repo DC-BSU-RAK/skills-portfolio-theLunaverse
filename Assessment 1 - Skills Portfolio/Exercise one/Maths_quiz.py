16f1bf986298578e5ee164a79a2373953d92f3ad
Luna Alsalman <manahel1506@gmail.com>
2025-11-03 22:41:47 +0400

    adding menu elements to maths quiz
    in order to get the paths for every media element in the menu of the quiz, i imported os module and used os.path.join.

--- Assessment 1 - Skills Portfolio/Exercise one/Maths_quiz.py
+++ Assessment 1 - Skills Portfolio/Exercise one/Maths_quiz.py
@@ -1,4 +1,15 @@
 import tkinter as tk
+import os
+
+# === File Paths ===
+
+# get the directory where this script is located
+BASE_DIR = os.path.dirname(__file__)
+
+# getting paths to all media files (backgrounds and buttons) using os.path.join
+MENU_GIF_PATH = os.path.join(BASE_DIR, "media", "menu_wp.gif")
+START_IMG_PATH = os.path.join(BASE_DIR, "media", "start_btn.png")
+QUIT_IMG_PATH = os.path.join(BASE_DIR, "media", "quit_btn.png")
 # === Settings ===
 WINDOW_WIDTH = 960
 WINDOW_HEIGHT = 540
