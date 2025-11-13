89f3ea81d967d8ca1ae18562277dd61adc1b1593
Luna Alsalman <manahel1506@gmail.com>
2025-11-14 01:41:12 +0400

    main window settings
    Added constants for the main window including width, height, and title.

--- Assessment 1 - Skills Portfolio/Exercise two/modules/constants.py
+++ Assessment 1 - Skills Portfolio/Exercise two/modules/constants.py
@@ -4,6 +4,10 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 JOKES_FILE_PATH = os.path.join(BASE_DIR, "media", "randomJokes.txt")
 
+# === window Settings ===
+WINDOW_WIDTH = 960
+WINDOW_HEIGHT = 540
+WINDOW_TITLE = "sans. - Joke Teller"
 # === font Settings ===
 FONT_FAMILY = "Comic Sans MS"
 DIALOGUE_FONT_SIZE = 30
