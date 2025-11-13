2d20f9700210e2e49e6c0d4e7c3ac7d4d843d7f1
Luna Alsalman <manahel1506@gmail.com>
2025-11-14 01:33:35 +0400

    Add SansJokeApp class initialisation
    Created the SansJokeApp class to initialise the main window and app variables. Configured window properties like title, size, background colour, and non-resizable behaviour. Set up initial state variables for joke handling, animations, typing control, and music playback.

--- Assessment 1 - Skills Portfolio/Exercise two/Sans.py
+++ Assessment 1 - Skills Portfolio/Exercise two/Sans.py
@@ -8,4 +8,21 @@ from modules.constants import *
 
 root = tk.Tk()
 
+class SansJokeApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title(WINDOW_TITLE)
+        
+        # === initialiation ===
+        self.jokes = self.load_jokes() # load jokes 
+        self.root.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
+        self.root.config(bg=BG_COLOR)
+        self.root.resizable(False, False)  # prevent window resizing
+        self.current_joke = None  # stores the currently selected joke
+        self.animation_text = None  # dialogue typewriter animation
+        self.animation_comment = None  # sans comment typewriter animation
+        self.is_typing = False  # prevents button clicks during typing
+        self.is_music_playing = True  # tracks mute state
+        self.button_enabled = {'punchline': False, 'next': False}  # track which buttons are clickable
+        
 root.mainloop()
